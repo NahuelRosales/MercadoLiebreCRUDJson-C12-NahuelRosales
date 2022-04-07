811db88deb90a74a71b501bfdff4e7957a4b103a
Nahuel Rosales <nahuelrosales@outlook.com>
2022-04-07 01:01:46 -0300

    Finished CRUD 1

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const writeJson = dataBase => fs.writeFileSync(productsFilePath, JSON.stringify(dataBase), "utf-8")
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -28,26 +29,81 @@ const controller = {
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		res.render("product-create-form")
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		const {name, price, discount, category, description} = req.body;
+
+		let lastId = 1;
+
+		products.forEach(product => {
+			if(product.id > lastId){
+				lastId = product.id
+			}
+		});
+		let newProduct = {
+			id : lastId + 1,
+			name,
+			price,
+			discount,
+			category,
+			description,
+			image: "default-image.png"
+		}
+		products.push(newProduct)
+
+		writeJson(products)
+		
+		res.redirect("/products")
+		
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		let productId = +req.params.id;
+		let productToEdit = products.find(product => product.id ===productId);
+
+		res.render("product-edit-form", {
+			product: productToEdit
+		})
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		let productId = +req.params.id;
+		const {name, price, discount, category, description} = req.body;
+
+		products.forEach(product =>{
+			if(product.id === productId){
+				product.id = product.id,
+				product.name = name,
+				product.price = price,
+				product.discount = +discount,
+				product.category = category,
+				product.description = description,
+				product.image = product.image
+			}
+		})
+
+		writeJson(products)
+
+		res.redirect(`/products/detail/${productId}`)
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		let productId = +req.params.id;
+
+		products.forEach(product => {
+			if(product.id === productId){
+				let productToDestroyIndex = products.indexOf(product)
+				productToDestroyIndex !== -1 ? products.splice(productToDestroyIndex, 1) : console.log("No encontré el producto")
+
+			}
+		})
+		writeJson(products);
+		res.send(`Eliminaste el producto con id ${productId}`)
 	}
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -9,20 +9,20 @@ const productsController = require('../controllers/productsController');
 router.get('/', productsController.index); 
 
 /*** CREATE ONE PRODUCT ***/ 
-//router.???('/???/', productsController.create); 
-//router.???('/', productsController.store); 
+router.get('/create', productsController.create); 
+router.post('/create', productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
 router.get('/detail/:id/', productsController.detail); 
 
 /*** EDIT ONE PRODUCT ***/ 
-//router.???('/:id/???', productsController.edit); 
-//router.???('/:id', productsController.update); 
+router.get('/:id/edit', productsController.edit); 
+router.put('/:id', productsController.update); 
 
 
 /*** DELETE ONE PRODUCT***/ 
-//router.???('/:id', productsController.destroy); 
+router.delete('/:id', productsController.destroy); 
 
 
 module.exports = router;
